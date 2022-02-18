9a81c8b60cc9e70c7526d049cca32cc84ab6638e
Mathieu DE SOUSA <m.desousa69150@gmail.com>
2022-02-18 19:09:34 +0100

    Ansible minimum configuration

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,11 @@
-output "static-ip" {
+output "instances-static-ip" {
     value = [
         for zone in var.zones : "${google_compute_instance.vault-server[zone].name}: ${google_compute_address.vault-static-ip[zone].address}"
+    ]
+}
+
+output "ansible-inventory" {
+    value = [
+        for zone in var.zones : google_compute_address.vault-static-ip[zone].address
     ]
 }
